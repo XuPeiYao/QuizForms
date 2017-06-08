ab53abbc1ee500c122a8ffe4983a6998b4cd585b
徐培堯 <st89382000@gmail.com>
2017-06-08 14:15:59 +0800

    實作測試Model

--- QuizForms/Models/Form.cs
+++ QuizForms/Models/Form.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 namespace QuizForms.Models
 {
     public class Form : IForm<Guid> {
-        public Guid Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string OwnerId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool Enable { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool Anonymous { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool Rewriteable { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int Order { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string OwnerId { get; set; }
+        public bool Enable { get; set; }
+        public bool Anonymous { get; set; }
+        public bool Rewriteable { get; set; }
+        public int Order { get; set; }
     }
 }
--- QuizForms/Models/Record.cs
+++ QuizForms/Models/Record.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using QuizForms.Quiz.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+using EzCoreKit.Extensions;
+
 namespace QuizForms.Models
 {
     public class Record : IRecord<Guid> {
-        public Guid Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public Guid FormId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public Guid QuestionId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string UserId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string Value { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public DateTime Time { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Guid Id { get; set; }
+        public Guid FormId { get; set; }
+        public Guid QuestionId { get; set; }
+        public string UserId { get; set; }
+        public string Value { get; set; }
+
+        [NotMapped]
+        [JsonIgnore]
+        public DateTime Time {
+            get {
+                return DateTimeFactory.ConvertFromJsTime(JsTime);
+            }
+            set {
+                JsTime = DateTimeFactory.ConvertToJsTime(value);
+            }
+        }
+
+        [Column("time")]
+        public long JsTime { get; set; }
     }
 }
