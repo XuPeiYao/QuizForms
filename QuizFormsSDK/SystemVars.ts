1d1c0b55d48f90f431fdc91d68174c66ef572619
徐培堯 <st89382000@gmail.com>
2017-06-09 13:12:37 +0800

    加入TypescriptSDK專案

---
+++ QuizFormsSDK/SystemVars.ts
@@ -0,0 +1,15 @@
+﻿module QuizForms {
+    export var SystemVars = {
+        apiUrl: location.origin + "/api/",
+        onException: (e) => { },
+        disableException: false
+    };
+
+    export function loadFromJSON(type: any, json: any): any {
+        var result = new type;
+        for (var key in json) {
+            result[key] = json[key];
+        }
+        return result;
+    }
+}
---
+++ QuizFormsSDK/User.ts
@@ -0,0 +1,42 @@
+﻿module QuizForms {
+    export enum UserTypes {
+        /**
+         * 未登入
+         */
+        Null = -1,
+        /**
+         * 未登入
+         */
+        Normal = 0,
+        /**
+         * 管理者
+         */
+        Admin = 1,
+        /**
+         * 系統管理者
+         */
+        TopAdmin = 2,
+    }
+    export class User {
+        /**
+         * 使用者類型
+         */
+        public type: UserTypes;
+
+        /**
+         * 使用者唯一識別號
+         */
+        public id: UserTypes;
+
+        /**
+         * 登入
+         * @param id
+         * @param password
+         */
+        public static async login(id: string, password: string): Promise<User> {
+            var client = new HttpClient();
+            var response = await client.postAsync("");
+            
+        }
+    }
+}
