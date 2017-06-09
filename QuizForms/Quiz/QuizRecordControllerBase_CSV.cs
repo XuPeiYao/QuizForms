793e3a932ed2ebfeee92a49561e7377904f4e8b0
徐培堯 <st89382000@gmail.com>
2017-06-09 11:05:47 +0800

    實作問卷結果之Excel與CSV下載

---
+++ QuizForms/Quiz/QuizRecordControllerBase_CSV.cs
@@ -0,0 +1,135 @@
+﻿using EzCoreKit.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using QuizForms.Quiz.Factories;
+using QuizForms.Quiz.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizForms.Quiz {
+    public partial class QuizRecordControllerBase<IdType, FormType, QuestionType, RecordType> {
+        public async Task<FileStreamResult> DownloadCSV(
+            FormType form,
+            long start = 0,
+            long end = -1) {
+            DateTime startDateTime = DateTimeFactory.ConvertFromJsTime(start);
+
+            if (end == -1) end = Database.Records.Where(x => x.FormId.Equals(form.Id)).Max(x => x.Time) + 1;
+
+            DateTime endDateTime = DateTimeFactory.ConvertFromJsTime(end);
+
+            return this.File(
+                await GetCSV(form, start, end),
+                "text/csv",
+                $"新生問卷紀錄 - {form.Name}({startDateTime.AddHours(8).ToString("yyyy-MM-dd HHmmss")}至{endDateTime.AddHours(8).ToString("yyyy-MM-dd HHmmss")}).csv");
+        }
+
+        public async Task<Stream> GetCSV(
+            FormType form,
+            long start = 0,
+            long end = -1) {
+            //寫過這張問卷的使用者
+            string[] writedUsers = Database.Records
+                .Where(x => x.FormId.Equals(form) && x.Time >= start && x.Time < end)
+                .Select(x => x.UserId)
+                .Distinct()
+                .ToArray();
+
+            var questions = Database
+                .Questions
+                .Where(x => x.FormId.Equals(form) && x.ParentId == null && x.IsQuestion())
+                .OrderBy(x => x.Order).ToArray();
+
+            List<IQuestion<IdType>> GetAllOptions(IQuestion<IdType> q)
+            {
+                List<IQuestion<IdType>> result_ = new List<IQuestion<IdType>>();
+                switch (q.Type) {
+                    case QuestionTypes.Radio:
+                        if (q.Children.Length == 0) break;
+                        foreach (var c in q.Children) {
+                            result_.AddRange(GetAllOptions(c));
+                        }
+                        break;
+                    case QuestionTypes.Checkbox:
+                        result_.Add(q);
+                        foreach (var c in q.Children) {
+                            result_.AddRange(GetAllOptions(c));
+                        }
+                        break;
+                    case QuestionTypes.InputText:
+                    case QuestionTypes.InputNumber:
+                    case QuestionTypes.Level://直接數值
+                        result_.Add(q);
+                        break;
+                    case QuestionTypes.Dropdown://單選
+                    case QuestionTypes.RadioGroup:
+                        result_.Add(q);
+                        #region 檢查展開項目
+                        foreach (var c in q.Children) {
+                            result_.AddRange(GetAllOptions(c));
+                        }
+                        #endregion
+                        break;
+                    case QuestionTypes.CheckboxGroup:
+                        #region 檢查展開項目
+                        foreach (var c in q.Children) {
+                            result_.AddRange(GetAllOptions(c));
+                        }
+                        #endregion
+                        break;
+                }
+                return result_;
+            }
+
+            var fullOptions = questions.SelectMany(x => GetAllOptions(x));
+
+            MemoryStream result = new MemoryStream();
+            StreamWriter writer = new StreamWriter(result, Encoding.UTF8);
+
+            writer.WriteLine("\"帳號\"," + string.Join(",", fullOptions.Select(x => '"' + x.Text.Replace("\"", "\"\"") + '"')));
+
+            writer.Flush();
+
+            var records = Database.Records.Where(x => x.FormId.Equals(form.Id)).ToArray();
+            foreach (var user in writedUsers) {//一個使用者一行
+                writer.Write(user);//userid
+
+                foreach (var question in fullOptions) {//列舉
+                    var record = records.FirstOrDefault(x => x.UserId == user && x.QuestionId.Equals(question.Id));
+                    string value = string.Empty;
+                    switch (question.Type) {
+                        case QuestionTypes.Checkbox:
+                            bool @checked = record != null;
+                            value = @checked ? "1" : "0";
+                            break;
+                        case QuestionTypes.InputText:
+                        case QuestionTypes.InputNumber:
+                        case QuestionTypes.Level://直接數值
+                            value = record?.Value ?? "";
+                            break;
+                        case QuestionTypes.Dropdown://單選
+                        case QuestionTypes.RadioGroup:
+                            var selected = question
+                                .Children
+                                .Select((x, i) => new { index = i, obj = x })
+                                .FirstOrDefault(x => records.Any(y => y.QuestionId.Equals(x.obj.Id) && y.UserId == user));
+
+                            value = (selected.index + 1).ToString();
+                            break;
+                    }
+                    writer.Write(",\"" + value.Replace("\"", "\"\"") + '"');
+                }
+
+                writer.WriteLine();
+                writer.Flush();
+            }
+
+
+            result.Position = 0;
+            return result;
+        }
+    }
+}
---
+++ QuizForms/Quiz/QuizRecordControllerBase_DownloadExcel.cs
@@ -0,0 +1,183 @@
+﻿using EzCoreKit.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using QuizForms.Quiz.Factories;
+using QuizForms.Quiz.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizForms.Quiz {
+    public partial class QuizRecordControllerBase<IdType, FormType, QuestionType, RecordType> {
+        public async Task<FileStreamResult> DownloadExcel(
+            FormType form,
+            long start = 0,
+            long end = -1) {
+            DateTime startDateTime = DateTimeFactory.ConvertFromJsTime(start);
+            
+            if (end == -1) end = Database.Records.Where(x=>x.FormId.Equals(form.Id)).Max(x => x.Time) + 1;
+            
+            DateTime endDateTime = DateTimeFactory.ConvertFromJsTime(end);
+
+            return this.File(
+                await GetExcel(form, start, end),
+                "text/csv",
+                $"新生問卷紀錄 - {form.Name}({startDateTime.AddHours(8).ToString("yyyy-MM-dd HHmmss")}至{endDateTime.AddHours(8).ToString("yyyy-MM-dd HHmmss")}).xlsx");
+        }
+
+        public async Task<Stream> GetExcel(
+            FormType form,
+            long start,
+            long end = -1) {
+            ExcelPackage excelPackage = new ExcelPackage();
+            ExcelWorkbook workbook = excelPackage.Workbook;
+            int qIndex = 1;
+
+            //寫過這張問卷的使用者
+            string[] writedUsers = Database.Records
+                .Where(x => x.FormId.Equals(form.Id) && x.Time >= start && x.Time < end)
+                .Select(x => x.UserId)
+                .Distinct()
+                .ToArray();
+
+            foreach (var question in Database
+                .Questions
+                .Where(x => x.FormId.Equals(form.Id) && x.ParentId == null && x.IsQuestion())
+                .OrderBy(x => x.Order).ToArray()) {
+                //以題目名稱作為工作表名稱
+                ExcelWorksheet worksheet = workbook.Worksheets.Add($"問題{qIndex++}");
+
+                worksheet.Cells[1, 1].Value = "題目內容:";
+                worksheet.Cells[1, 2].Value = question.Text;
+                worksheet.Cells[2, 1].Value = "題目類型:";
+                worksheet.Cells[2, 2].Value = question.Type;
+                worksheet.Column(1).Width = 14;
+
+                List<IQuestion<IdType>> twoLevelAfterOptions = new List<IQuestion<IdType>>();
+                void ListOptions(IQuestion<IdType> q, string id, ref int rowPosition){
+                    for (int i = 0; i < q.Children.Length; i++) {
+                        var temp = "";
+                        if (id == null) {
+                            worksheet.Cells[rowPosition, 1].Value = temp = (i + 1).ToString();
+                        } else {
+                            worksheet.Cells[rowPosition, 1].Value = temp = id + "_" + (i + 1);
+                            twoLevelAfterOptions.Add(q.Children[i]);
+                        }
+                        worksheet.Cells[rowPosition, 2].Value = q.Children[i].Type;
+                        worksheet.Cells[rowPosition, 3].Value = q.Children[i].Text;
+                        rowPosition++;
+                        if (q.Children[i].Children?.Length > 0) {
+                            ListOptions(q.Children[i], temp, ref rowPosition);
+                        }
+                    }
+                }
+
+                List<IQuestion<IdType>> GetAllOptions(IQuestion<IdType> q){
+                    List<IQuestion<IdType>> result = new List<IQuestion<IdType>>();
+                    for (int i = 0; i < q.Children.Length; i++) {
+                        result.Add(q.Children[i]);
+                        if (q.Children[i].Children?.Length > 0) {
+                            result.AddRange(GetAllOptions(q.Children[i]));
+                        }
+                    }
+                    return result;
+                }
+
+                int row = 3;
+                if (question.Type == QuestionTypes.RadioGroup ||
+                    question.Type == QuestionTypes.Dropdown
+                    /*question.QuestionType == QuestionTypes.BooleanGroup*/) {
+                    worksheet.Cells[row, 1].Value = "選項編號";
+                    worksheet.Cells[row, 2].Value = "選項型別";
+                    worksheet.Cells[row, 3].Value = "選項內容";
+                    row++;
+
+                    ListOptions(question, null, ref row);
+                }
+
+                var allOptions = GetAllOptions(question);
+
+                worksheet.Cells[row, 1].Value = "帳號";
+                if (question.Type != QuestionTypes.CheckboxGroup) {
+                    worksheet.Cells[row, 2].Value = "值";
+                    for (int col = 0; col < twoLevelAfterOptions.Count; col++) {
+                        worksheet.Cells[row, col + 3].Value = twoLevelAfterOptions[col].Text;
+                    }
+                } else {
+                    for (int col = 0; col < allOptions.Count; col++) {
+                        worksheet.Cells[row, col + 2].Value = allOptions[col].Text;
+                    }
+                }
+                row++;
+
+                worksheet.View.FreezePanes(row, 1);
+
+
+
+                if (question.Type == QuestionTypes.CheckboxGroup) {
+                    for (int userIndex = 0; userIndex < writedUsers.Length; userIndex++) {
+                        worksheet.Cells[row, 1].Value = writedUsers[userIndex];
+                        for (int col = 0; col < allOptions.Count; col++) {
+                            var record = (from t in Database.Records
+                                          where t.QuestionId.Equals(allOptions[col].Id) &&
+                                               t.UserId == writedUsers[userIndex]
+                                          select t).FirstOrDefault();
+
+                            if (record == null) {
+                                if (allOptions[col].Type == QuestionTypes.Checkbox) {
+                                    worksheet.Cells[row, col + 2].Value = "0";
+                                }
+                            } else {
+                                if (allOptions[col].Type == QuestionTypes.Checkbox) {
+                                    worksheet.Cells[row, col + 2].Value = "1";
+                                } else {
+                                    worksheet.Cells[row, col + 2].Value = record.Value;
+                                }
+                            }
+                        }
+                        row++;
+                    }
+                } else {
+                    for (int userIndex = 0; userIndex < writedUsers.Length; userIndex++) {
+                        worksheet.Cells[row, 1].Value = writedUsers[userIndex];
+
+                        if (question.Type == QuestionTypes.RadioGroup ||
+                            question.Type == QuestionTypes.Dropdown) {
+                            for (int optIndex = 0; optIndex < question.Children.Length; optIndex++) {
+                                var record = (from t in Database.Records
+                                              where t.QuestionId.Equals(question.Children[optIndex].Id) &&
+                                                    t.UserId == writedUsers[userIndex]
+                                              select t).FirstOrDefault();
+                                if (record == null) continue;
+                                worksheet.Cells[row, 2].Value = optIndex + 1;
+                            }
+
+                            for (int optIndex = 0; optIndex < twoLevelAfterOptions.Count; optIndex++) {
+                                var record = (from t in Database.Records
+                                              where t.QuestionId.Equals(twoLevelAfterOptions[optIndex].Id) &&
+                                                    t.UserId == writedUsers[userIndex]
+                                              select t).FirstOrDefault();
+                                if (record == null) continue;
+                                worksheet.Cells[row, 3 + optIndex].Value = record.Value;
+                            }
+
+                        } else {//input
+                            var record = (from t in Database.Records
+                                          where t.QuestionId.Equals(question.Id) &&
+                                                t.UserId == writedUsers[userIndex]
+                                          select t).FirstOrDefault();
+                            worksheet.Cells[row, 2].Value = record?.Value;
+                        }
+
+                        row++;
+                    }
+                }
+
+            }
+
+            return new MemoryStream(excelPackage.GetAsByteArray());
+        }
+    }
+}
