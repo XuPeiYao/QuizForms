4e8b827af4c866b60a5c758724155157db16ad05
徐培堯 <st89382000@gmail.com>
2017-06-12 10:32:58 +0800

    加入location.origin不存在的修正

--- QuizFormsSDK/SystemVars.ts
+++ QuizFormsSDK/SystemVars.ts
@@ -1,9 +1,24 @@
 ﻿module QuizForms {
     export var SystemVars = {
-        apiUrl: location.origin + "/api/",
+        get origin(): string {
+            if (!window.location.origin) {
+                (<any>(window.location)).origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
+            }
+            return location.origin;
+        },
+        get apiUrl(): string {
+            return location.origin + "/api/";
+        },
         onException: (e) => { },
         disableException: false
     };
+
+    export function createHttpClient(): HttpClient {
+        var client = new HttpClient();
+        client.withCredentials = true;
+
+        return client;
+    }
 
     export function loadFromJSON(type: any, json: any): any {
         var result = new type;
